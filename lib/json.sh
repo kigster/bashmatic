1adbd881cddebee7cde1300f96a8cbfb146031f2
Konstantin Gredeskoul <kig@flowinc.com>
2019-01-07 12:37:05 -0800

    Adding lib::json

---
+++ lib/json.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+#
+# (c) 2019 Konstantin Gredeskoul, MIT License.
+#
+# Usage Example (Generating JSON Certificates)
+# ============================================
+#
+# DOMAIN="my.awesome.domain"
+# lib::json::begin-hash
+#  lib::json::begin-hash "${DOMAIN/star/*}"
+#    lib::json::file-to-array "key"         "${DOMAIN}.key"        true
+#    lib::json::file-to-array "certificate" "${DOMAIN}.crt"        true
+#    lib::json::file-to-array "chain"       "${DOMAIN}.chain"
+#  lib::json::end-hash
+# lib::json::end-hash
+ 
+function lib::json::begin-array() {
+  [[ -n "$1" ]] && lib::json::begin-key "$1"
+  echo " ["
+}
+
+function lib::json::end-array() {
+  printf "]"
+  [[ "$1" == "true" ]] && printf ","
+  echo
+}
+
+function lib::json::file-to-array() {
+  lib::json::begin-array "$1"
+  cat $2 |  \
+    tr -d '\r' | \
+    tr -d '\015' | \
+    sed 's/^/"/g;s/$/",/g' | \
+    tail -r | \
+    awk -F, '{if (FNR!=1) print; else print $1} ' | \
+    tail -r
+
+  lib::json::end-array $3
+}
+
+function lib::json::begin-key() {
+  if [[ -n "$1" ]]; then
+    printf "\"${1}\": "
+  fi
+}
+
+function lib::json::begin-hash() {
+  [[ -n "$1" ]] && lib::json::begin-key "$1"
+  echo "{"
+}
+
+function lib::json::end-hash() {
+  printf "}"
+  [[ "$1" == "true" ]] && printf ","
+  echo
+}
