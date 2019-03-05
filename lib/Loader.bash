0ce86a81132959ca0355efee6b9bc2805d2d83c5
Konstantin Gredeskoul <kig@flowinc.com>
2019-03-05 12:40:00 -0800

    Try ~/.bashmatic before giving up

--- lib/Loader.bash
+++ lib/Loader.bash
@@ -3,14 +3,32 @@
 export True=1
 export False=0
 
+# Set this externally if your bashmatic is not installed in ~/.bashmatic
+export BashMatic__Home=${BashMatic__Home:-"${HOME}/.bashmatic"}
+
 export BashMatic__SearchTarget="Loader.bash"
 export BashMatic__Loader=$(find -L . -maxdepth 3 -type f -name "${BashMatic__SearchTarget}" -print 2>/dev/null | tail -1)
 
 [[ -z ${BashMatic__Downloader} && -n $(which curl) ]] && export BashMatic__Downloader="curl -fsSL --connect-timeout 5 "
 [[ -z ${BashMatic__Downloader} && -n $(which wget) ]] && export BashMatic__Downloader="wget -q -O --connect-timeout=5 - "
 
-if [[ -z ${BashMatic__Loader} ]]; then
-  printf "${bldred}ERROR: ${clr}Can not find ${bldylw}${BashMatic__SearchTarget}${clr} file, aborting."
+[[ -d "${BashMatic__Home}" && -z ${BashMatic__Loader} ]] && \
+  export BashMatic__Loader=$(find "${BashMatic__Home}" -maxdepth 3 -type f -name "${BashMatic__SearchTarget}" -print 2>/dev/null | tail -1)
+
+if [[ -z "${BashMatic__Loader}" ]]; then
+  printf "${bldred}ERROR: ${clr}Can not find ${bldylw}${BashMatic__SearchTarget}${clr} file, aborting.\n\n"
+
+  printf "${bldblu}If you installed BashMatic not in ~/.bashmatic (it's default home), \n"
+  printf "you might consider setting environment variable ${bldylw}BashMatic__Home${bldblu} \n"
+  printf "like so:\n\n"
+  
+  printf "   ${bldylw}echo 'export BashMatic__Home=/Users/kig/.scripts/bashmatic' >> ~/.bash_profile${clr}\n\n"
+
+  printf "${bldblu}Have you installed BashMatic using the bootstrap script? If not,\n"
+  printf "Run the following command:\n\n"
+
+  printf "   ${bldylw}cd ~/; curl -fsSL http://bit.ly/bashmatic-bootstrap | /usr/bin/env bash${clr}\n\n"
+
   (( ${__ran_as_script} )) && exit 1 || return 1
 fi
 
