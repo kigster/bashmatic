180b87ef5a9242d8fe5de53c05e47b69e77aa926
Konstantin Gredeskoul <konstantin.gredeskoul@coinbase.com>
2019-11-01 14:56:58 -0700

    Fixing wav2mp3 and adding æ prefix

--- lib/audio.sh
+++ lib/audio.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 lib::audio::wav-to-mp3() {
-  local file="$1"
+  local file="$1"; shift
   [[ -z "${file}" ]] && { 
-    hl::subtle "USAGE: wav2mp3 <file.wav>"
+    h2 "USAGE: wav2mp3 <file.wav>" \
+       "NOTE: wave file sampling rate will be auto-detected."
     return
   }
      
@@ -11,12 +12,41 @@ lib::audio::wav-to-mp3() {
 
   nfile=$(echo "${file}" | sed -E 's/\.wav$/\.mp3/ig')
 
-  info "Converting file ${bllldylw}}$(basename "${file}")$(txt-info) to ${bldylw}${nfile}..." 
+  khz=$(lib::audio::wave-file-frequency "${file}")
+
+  info "${bldgrn}Source: ${bldylw}$(basename "${file}")" 
+  info "${bldpur}Output: ${bldylw}${nfile}$(txt-info) | (sampling rate: ${bldgrn}${khz:-'Unknown'}kHz)"
+
+  [[ -n ${khz} ]] && khz=" -s ${khz} "
   run::set-next show-output-on
-  lame --disptime 1 -m s -r -q 0 -b 320 --cbr "${file}" "${nfile}"
+
+  hr
+  run "lame --disptime 1 -m s -r -q 0 -b 320 ${khz} --cbr $* ${file} ${nfile}"
+  hr
 }
 
+lib::audio::wave-file-frequency() {
+  local file="$1"
 
-wave2mp3() {
+  [[ -z $(which mdls) ]] && return 1
+
+  local frequency=$(mdls ${file} | grep kMDItemAudioSampleRate | sed 's/.*= //g')
+  local kHz=$(( ${frequency} / 1000 ))
+  printf ${kHz}
+}
+
+function æ-wav2mp3() {
   lib::audio::wav-to-mp3 "$@"
 }
+
+function æ-wavfreq() {
+  [[ -f ${1} ]] || { 
+    error "File ${1} does not exist."
+    return 1
+  }
+
+  lib::audio::wave-file-frequency "$@"
+  echo " kHz"
+}
+
+
