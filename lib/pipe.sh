4518b55e6cf804bcae037dad90ed5bd8d0714690
Konstantin Gredeskoul <kigster@gmail.com>
2020-05-28 19:56:06 -0700

    Fix test

--- lib/pipe.sh
+++ lib/pipe.sh
@@ -7,13 +7,13 @@
 # VAR=VALUE
 # returns just the variable names
 pipe.extract-variables() {
-  sedx -E '/^\(\s*\)#.*$/D;/^\s*$/D;/^[^A-Z].*$/D;s/export //gi;' | cut -d '=' -f 1
+  sedx '/^\(\s*\)#.*$/D; /^\s*$/D; /^[^A-Z].*$/D' | sedx 's/export //ig' | cut -d '=' -f 1
 }
 
 pipe.remove-hash-comments() {
-  sedx -E '/^\(\s*\)?#.*$/D; s/#.*$//g;'
+  sedx '/^\(\s*\)?#.*$/D; s/#.*$//g;'
 }
 
 pipe.remove-blank-lines() {
-  sedx -E '/^$/D'
+  sedx '/^$/D'
 }
