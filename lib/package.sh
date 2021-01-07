c04baf96db047b31fa153171b239d3e57a98d0bd
Konstantin Gredeskoul <kigster@gmail.com>
2021-01-07 04:33:35 -0800

    WIP: linux installer

---
+++ lib/package.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# vim: ft=bash
+
+function package.install() {
+
+}
+
+function package.uninstall() {
+}
+
+function package.package.is-installed() {
+}
+
+function package.package.all-installed() {
+} 
+
+function package.reinstall.package() {
+} 
+
+function package.install.package() {
+} 
+
+function package.uninstall.package() {
+} 
+
+function package.install.packages() {
+} 
+
+function package.reinstall.packages() {
+} 
+
+function package.uninstall.packages() {
+
+}
