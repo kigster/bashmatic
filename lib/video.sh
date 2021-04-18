6ee7ab81f08131be66fe45f7724d151a85fef2a2
Konstantin Gredeskoul <kigster@gmail.com>
2021-04-18 01:24:10 -0700

    Adding more video compressions

--- lib/video.sh
+++ lib/video.sh
@@ -18,22 +18,59 @@
   return 1
 }
 
+# ffmpeg -i input.mkv -vf "scale=iw/2:ih/2" half_the_frame_size.mkv
+# ffmpeg -i input.mkv -vf "scale=iw/3:ih/3" a_third_the_frame_size.mkv
+# ffmpeg -i input.mkv -vf "scale=iw/4:ih/4" a_fourth_the_frame_size.mkv
+
+.video.convert.compress-11() {
+  run "ffmpeg -n -loglevel error -stats -i \"${1}\" -preset faster -c:v libx265 -crf 22 -c:a copy \"${2}\""
+}
+
+.video.convert.compress-12() {
+  run "ffmpeg -n -loglevel error -stats -i \"${1}\" -preset faster -c:v libx265 -crf 22 -c:a copy -vf 'scale=iw/2:ih/2' \"${2}\""
+}
+
+.video.convert.compress-13() {
+  run "ffmpeg -n -loglevel error -stats -i \"${1}\" -preset faster -c:v libx265 -crf 22 -c:a copy -vf 'scale=iw/3:ih/3' \"${2}\""
+}
+
+.video.convert.compress-21() {
+  run "ffmpeg -n -loglevel error -stats -i \"${1}\" -preset faster -vcodec libx264 -crf 28 -tune film \"${2}\""
+}
+
+.video.convert.compress-22() {
+  run "ffmpeg -n -loglevel error -stats -i \"${1}\" -preset faster -vcodec libx264 -crf 28 -tune film -vf 'scale=iw/2:ih/2' \"${2}\""
+}
+
+.video.convert.compress-23() {
+  run "ffmpeg -n -loglevel error -stats -i \"${1}\" -preset faster -vcodec libx264 -crf 28 -tune film -vf 'scale=iw/3:ih/3' \"${2}\""
+}
+
+.video.convert.compress-3() {
+  # https://unix.stackexchange.com/questions/28803/how-can-i-reduce-a-videos-size-with-ffmpeg
+  # Here is a 2 pass example. Pretty hard coded but it really puts on the squeeze
+  run "ffmpeg -y -i \"$1\" -c:v libvpx-vp9 -pass 1 -deadline best -crf 30 -b:v 664k -c:a libopus -f webm /dev/null && ffmpeg -i \"$1\" -c:v libvpx-vp9 -pass 2 -crf 30 -b:v 664k -c:a libopus -strict -2 \"$2\""
+}
+
 video.convert.compress() {
   local file="$1"; shift
   local output=${file/\.*/.mkv}
-  local ratio="${2:-"1"}"
+  local algo="${2:-"1"}"
 
   [[ "${file}" == "${output}" ]] && output="${output/\.mkv/-smaller-${ratio}.mkv}"
   
-  h2 "Starting ffmpeg conversion from ${file} to ${output}" \
+  h2 "Starting ffmpeg conversion from ${file} to ${output}, with algorithm #${ratio}" \
      "Source file size is $(file.size.mb ${file})"
   
   run.set-next show-output-on
-  run "ffmpeg -stats -y -i ${file} -preset:v veryfast -loglevel error -c:v libx265 -crf 22 -c:a copy -vf 'scale=iw/3:ih/3' ${output}"
+  local func=".video.convert.compress-${algo}"
+  run "${func} \"${file}\" \"${output}\""
   
-  local before=$(file.size ${file})
-  local after=$(file.size ${output})
+  local before="$(file.size "${file}")"
+  local after="$(file.size "${output}")"
   local reduction=$(( 100 * ( before - after ) / before ))
 
   success "File ${output} was prodiced with ${reduction}% size savings."  
 }
+
+
