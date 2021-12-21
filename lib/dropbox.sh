5eeb3c194892d1db2f2dfb146dd5f37a905978e4
Konstantin Gredeskoul <kigster@gmail.com>
2021-12-21 15:00:23 -0800

    Adding dropbox ignore/unignore functions (#91)

---
+++ lib/dropbox.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# vim: ft=bash
+# @description Utilities relating to Dropbox
+#
+
+# @description Set file to be ignored by Dropbox
+# @see https://help.dropbox.com/files-folders/restore-delete/ignored-files
+function dropbox.ignore {
+  local file="$(path.absolute "$1")"
+  util.os
+
+  case "${AppCurrentOS}" in
+    darwin)
+      run "xattr -w com.dropbox.ignored 1 \"${file}\""
+      ;;
+    linux)
+      run "attr -s com.dropbox.ignored -V 1  \"${file}\""
+      ;;
+  esac
+} 
+
+# @description Set a file or directorhy to be ignored by Dropbox
+# @see https://help.dropbox.com/files-folders/restore-delete/ignored-files
+function dropbox.unignore() {
+  local file="$(path.absolute "$1")"
+  util.os
+  case "${AppCurrentOS}" in
+    darwin)
+      run "xattr -d com.dropbox.ignored \"${file}\""
+      ;;
+    linux)
+      run "attr -r com.dropbox.ignored \"${file}\""
+      ;;
+  esac
+}
--- lib/path.sh
+++ lib/path.sh
@@ -165,3 +165,19 @@ function path.mutate.prepend() {
 function PATH_add() {
   path.mutate.append "$@"
 }
+
+# @description Returns an absolute version of a given path
+function path.absolute() {
+  if [[ -d "$1" ]]; then
+      pushd "$1" >/dev/null
+      pwd
+      popd >/dev/null
+  elif [[ -e "$1" ]]; then
+      pushd "$(dirname "$1")" >/dev/null
+      echo "$(pwd)/$(basename "$1")"
+      popd >/dev/null
+  else
+      echo "$1" does not exist! >&2
+      return 127
+  fi
+}
